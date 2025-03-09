0b3f9b637127cbf8b1d0a217f4ffb5a7e7c91795
PramitaDash <dashpramita34@gmail.com>
2025-03-09 23:45:36 +0530

    first commit

---
+++ src/main/java/com/psa/psa_git_project/PsaGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.psa.psa_git_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaGitProjectApplication.class, args);
+	}
+
+}
