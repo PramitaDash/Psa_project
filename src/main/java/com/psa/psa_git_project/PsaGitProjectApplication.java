4d6bd08ffeb16e55883108e66fe46e3ea6bf4534
Pramita Dash <147218423+PramitaDash@users.noreply.github.com>
2025-03-09 23:48:16 +0530

    Update PsaGitProjectApplication.java
    update

--- src/main/java/com/psa/psa_git_project/PsaGitProjectApplication.java
+++ src/main/java/com/psa/psa_git_project/PsaGitProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PsaGitProjectApplication {
 
+	int x = 100;
+
 	public static void main(String[] args) {
 		SpringApplication.run(PsaGitProjectApplication.class, args);
 	}
